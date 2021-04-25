9c88ce98ec1f97a7db7a211de4238823f3d009fa
shaylevin89 <50113597+shaylevin89@users.noreply.github.com>
2021-04-25 12:32:21 +0300

    final

--- app.py
+++ app.py
@@ -23,6 +23,17 @@ def get_args(request_args):
     return lon, lat
 
 
+def lon_lat_check_and_round(lon, lat):
+    try:
+        lon = round(float(lon)*2)/2
+        lat = round(float(lat)*2)/2
+        if -180 <= lon <= 180 and -90 <= lat <= 90:
+            return lon, lat
+        return None, None
+    except Exception:
+        return None, None
+
+
 app = Flask(__name__)
 
 
